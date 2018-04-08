94f06ff881ccfd36bdc58ed800fb39fa307689f6
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-04-08 18:28:09 -0500

    Cambios para la busqueda en navegador

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,7 @@
 class ExpensesController < ApplicationController
   def index
      @expenses = Expense.order("date DESC")
+     @expenses = @expenses.select{|x| x[:concept].downcase.gsub("á", "a").include?(params[:concept].downcase.gsub("á", "a"))} if params[:concept].present?
+     @expenses = @expenses.select{|x| x[:category_id].to_s.eql?(params[:category_id])} if params[:category_id].present?
   end
 end
