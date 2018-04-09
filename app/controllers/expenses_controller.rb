32e12d3073fd7b978deff3bc8bea215d9aad2531
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-04-08 20:26:18 -0500

    Cambios

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,8 +2,9 @@ class ExpensesController < ApplicationController
 before_action :authenticate_user!, only: [:index]
   def index
 
-     @expenses = Expense.order("date DESC")
-     @expenses = @expenses.user.select{|x| x[:concept].downcase.gsub("á", "a").include?(params[:concept].downcase.gsub("á", "a"))} if params[:concept].present?
-     @expenses = @expenses.user.select{|x| x[:category_id].to_s.eql?(params[:category_id])} if params[:category_id].present?
+     @expenses = current_user.expenses.order("date DESC")
+
+     @expenses = @expenses.select{|x| x[:concept].downcase.gsub("á", "a").include?(params[:concept].downcase.gsub("á", "a"))} if params[:concept].present?
+     @expenses = @expenses.select{|x| x[:category_id].to_s.eql?(params[:category_id])} if params[:category_id].present?
   end
 end
