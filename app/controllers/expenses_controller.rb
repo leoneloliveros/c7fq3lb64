8c0d98dfde08d4881cc3b03c47e97d349f330aae
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-04-08 19:50:38 -0500

    Crear autenticacion

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,7 +1,9 @@
 class ExpensesController < ApplicationController
+before_action :authenticate_user!, only: [:index]
   def index
+
      @expenses = Expense.order("date DESC")
-     @expenses = @expenses.select{|x| x[:concept].downcase.gsub("á", "a").include?(params[:concept].downcase.gsub("á", "a"))} if params[:concept].present?
-     @expenses = @expenses.select{|x| x[:category_id].to_s.eql?(params[:category_id])} if params[:category_id].present?
+     @expenses = @expenses.user.select{|x| x[:concept].downcase.gsub("á", "a").include?(params[:concept].downcase.gsub("á", "a"))} if params[:concept].present?
+     @expenses = @expenses.user.select{|x| x[:category_id].to_s.eql?(params[:category_id])} if params[:category_id].present?
   end
 end
